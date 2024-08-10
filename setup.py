cdf51937cffaf0b08fedb0a2a9358c01b06fdb18
Madhabpoulik <madhabpoulik97@gmail.com>
2024-08-10 20:16:05 +0530

    fix14

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirement(file_path:str)->List[str]:
 with open("README.md","r",encoding="utf-8") as f:
     long_description = f.read()
     
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "MongoDB_connector"
 PKG_NAME = "MongoConn"
 AUTHOR_USER_NAME = "Madhabpoulik"  
